e0c9d9a0439c1d5a688609b6b9227d141762d57e
Timthony <tianhangz1023@gmail.com>
2018-05-28 09:39:40 +0800

    第一次添加

---
+++ cmake-build-debug/CMakeFiles/imgtoavi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgtoavi.dir/main.cpp.o"
+  "imgtoavi.pdb"
+  "imgtoavi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgtoavi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,47 @@
+//功能：将连续图片合成视频AVI
+//注意：检查路径是否存在空格，否则影响结果
+//注意：根据文件名设置循环，何时开始循环
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+
+cv::VideoWriter writer;
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    Mat src=imread("/Users/arcstone_mems_108/Desktop/result/自动抽核/次靠前/1/1.jpg",1);
+    if(!src.data)
+    {
+        cout<<"buxinga!"<<endl;
+    }
+    char image_name[200];
+    int isColor = 1;
+    int fps = 5;
+    int frameWidth = src.cols;
+    int frameHeight = src.rows;
+
+    writer = VideoWriter("/Users/arcstone_mems_108/Desktop/result/自动抽核/次靠前/1/test_1_1.avi",CV_FOURCC('M','J','P','G'), fps,
+                         Size(frameWidth, frameHeight), isColor);
+
+    cout << "info:" << endl
+         << "ff.avi" << endl
+         << "Size:" << frameWidth << "*" << frameHeight << endl
+         << "fps:" << fps << endl;
+
+
+    for (int i = 1; i < 1000; i++)
+    {
+        sprintf(image_name, "/Users/arcstone_mems_108/Desktop/result/自动抽核/次靠前/1/%d.jpg", i);
+        src = imread(image_name, 1);
+        if (src.empty())
+        {
+            cout<<" 全部图像加载完成！";
+            break;
+        }
+        imshow("【src】", src);
+        waitKey(5);
+        writer.write(src);
+    }
+}
